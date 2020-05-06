e5b0c4d9947d3cd7e85eb11b546d0ad7142c2b7c
Danny Guinther <dguinther@seismic.com>
2020-05-06 17:26:05 -0400

    Port to netcoreapp 3.1 and allow for async disposal

--- Snowflake.Data/Configuration/SFConfigurationSectionHandler.cs
+++ Snowflake.Data/Configuration/SFConfigurationSectionHandler.cs
@@ -11,20 +11,13 @@ namespace Snowflake.Data.Configuration
     /// <summary>
     ///     Develop In progress
     /// </summary>
-    public class SFConfigurationSectionHandler : IConfigurationSectionHandler
+    public class SFConfigurationSectionHandler : ConfigurationSection
     {
         static SFConfigurationSectionHandler()
         { }
 
         public SFConfigurationSectionHandler()
         { }
-
-        #region IConfigurationSectionHandler Memebers
-        object IConfigurationSectionHandler.Create(object parent, object configContext, XmlNode section)
-        {
-            return null;
-        }
-        #endregion
     }
 
 }
